39295eeeefcc3789d88d9cc4b10f8c3ee3170218
Mohamed Chérif Diallo <diallo.mohamed-cherif@ugb.edu.sn>
2023-12-10 17:59:42 +0000

    premier commit projet spring

---
+++ demo-cit-ipsl/src/test/java/sn/ipsl/demo/DemoCitIpslApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.ipsl.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCitIpslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
